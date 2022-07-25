ea21532a678a3704c2b0b4dc626b94402125274f
hantabaru1014 <hantabaru1014@gmail.com>
2022-07-25 20:55:17 +0900

    Add user favorite feature

--- NeosContactsSort/NeosContactsSort.cs
+++ NeosContactsSort/NeosContactsSort.cs
@@ -3,25 +3,35 @@
 using BaseX;
 using CloudX.Shared;
 using FrooxEngine;
+using FrooxEngine.UIX;
 using HarmonyLib;
 using NeosModLoader;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NeosContactsSort
 {
     public class NeosContactsSort : NeosMod
     {
         public override string Name => "NeosContactsSort";
-        public override string Author => "runtime";
-        public override string Version => "1.2.0";
-        public override string Link => "https://github.com/zkxs/NeosContactsSort";
+        public override string Author => "hantabaru1014";
+        public override string Version => "2.0.0";
+        public override string Link => "https://github.com/hantabaru1014/NeosContactsSort";
+
+        public static ModConfiguration? config;
+
+        [AutoRegisterConfigKey]
+        public static readonly ModConfigurationKey<List<string>> FavoriteUsers = new ModConfigurationKey<List<string>>("FavoriteUsers", " Favorite Users", () => new List<string>());
 
         public override void OnEngineInit()
         {
 #if DEBUG
             Warn($"Extremely verbose debug logging is enabled in this build. This probably means runtime messed up and gave you a debug build.");
 #endif
-            Harmony harmony = new Harmony("net.michaelripley.NeosContactsSort");
+            config = GetConfiguration();
+
+            Harmony harmony = new Harmony("net.hantabaru1014.NeosContactsSort");
             harmony.PatchAll();
         }
 
@@ -61,6 +71,11 @@ namespace NeosContactsSort
                         int messageComparison = -component1!.HasMessages.CompareTo(component2!.HasMessages);
                         if (messageComparison != 0) return messageComparison;
 
+                        // favorite users comes second
+                        var favUsers = config!.GetValue(FavoriteUsers);
+                        if (favUsers!.Contains(friend1!.FriendUserId) && !favUsers.Contains(friend2!.FriendUserId)) return -1;
+                        if (!favUsers.Contains(friend1.FriendUserId) && favUsers.Contains(friend2!.FriendUserId)) return 1;
+
                         // sort by online status
                         int onlineStatusOrder = GetOrderNumber(friend1!).CompareTo(GetOrderNumber(friend2!));
                         if (onlineStatusOrder != 0) return onlineStatusOrder;
@@ -98,6 +113,37 @@ namespace NeosContactsSort
                     __result = color.Yellow;
                 }
             }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(FriendsDialog), "UpdateSelectedFriend")]
+            public static void FriendsDialogUpdateSelectedFriendPostfix(FriendsDialog __instance, UIBuilder ___actionsUi)
+            {
+                if (__instance.SelectedFriend == null || __instance.SelectedFriend.FriendUserId == "U-Neos") return;
+                UIBuilder actionsUIBuilder = ___actionsUi;
+                var favUsers = config!.GetValue(FavoriteUsers)!;
+                if (favUsers.Contains(__instance.SelectedFriendId))
+                {
+                    actionsUIBuilder.Button("★").LocalPressed += (IButton button, ButtonEventData eventData) =>
+                    {
+                        favUsers.Remove(__instance.SelectedFriendId);
+                        config.Set(FavoriteUsers, favUsers);
+                        UpdateFriendsDialog(__instance);
+                    };
+                }
+                else
+                {
+                    actionsUIBuilder.Button("☆").LocalPressed += (IButton button, ButtonEventData eventData) =>
+                    {
+                        favUsers.Add(__instance.SelectedFriendId);
+                        config.Set(FavoriteUsers, favUsers);
+                        UpdateFriendsDialog(__instance);
+                    };
+                }
+                var favBtnSlot = actionsUIBuilder.Root.Children.Last();
+                favBtnSlot.OrderOffset = -1;
+                var el = favBtnSlot.GetComponent<LayoutElement>();
+                el.PreferredWidth.Value = 30f;
+            }
         }
 
         // lower numbers appear earlier in the list
@@ -129,6 +175,12 @@ namespace NeosContactsSort
                         return 6;
                     }
             }
+        }
+
+        private static void UpdateFriendsDialog(FriendsDialog instance)
+        {
+            AccessTools.Method(typeof(FriendsDialog), "UpdateSelectedFriend").Invoke(instance, null);
+            AccessTools.Method(typeof(FriendsDialog), "OnCommonUpdate").Invoke(instance, null);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # NeosContactsSort
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that sorts contacts Better™.
+A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that sorts contacts Better™.  
+And added user favorite feature. (This fork)
 
 Relevant Neos issue: [#2596](https://github.com/Neos-Metaverse/NeosPublic/issues/2596).
 
@@ -24,7 +25,8 @@ I've made a few noteworthy changes to the sorting order:
 
 ### Modded Sort
 1. Friends with unread messages
-2. Ties broken by online status
+2. Favorite Users
+3. Ties broken by online status
    1. Incoming Friend Requests
    2. Neos Bot
    3. Online Friends
@@ -33,9 +35,9 @@ I've made a few noteworthy changes to the sorting order:
    6. Sent Requests (background color changed from gray to yellow!)
    7. Offline Friends
    8. Search results
-3. Further ties broken by username alphabetical order
+4. Further ties broken by username alphabetical order
 
 ## Installation
 1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
-2. Place [NeosContactsSort.dll](https://github.com/zkxs/NeosContactsSort/releases/latest/download/NeosContactsSort.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
+2. Place [NeosContactsSort.dll](https://github.com/hantabaru1014/NeosContactsSort/releases/latest/download/NeosContactsSort.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
 3. Start the game. If you want to verify that the mod is working you can check your Neos logs.
