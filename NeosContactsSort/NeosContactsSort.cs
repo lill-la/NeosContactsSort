724f6fee196f8f5ad8ea06eda67e0011b42325cf
hantabaru1014 <hantabaru1014@gmail.com>
2023-12-25 01:28:15 +0900

    port to resonite

--- NeosContactsSort/NeosContactsSort.cs
+++ NeosContactsSort/NeosContactsSort.cs
@@ -1,34 +1,34 @@
-﻿//#define DEBUG // if true do a lot of debug spam
-
-using BaseX;
-using CloudX.Shared;
+﻿using SkyFrost.Base;
 using FrooxEngine;
 using FrooxEngine.UIX;
 using HarmonyLib;
-using NeosModLoader;
+using ResoniteModLoader;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace NeosContactsSort
 {
-    public class NeosContactsSort : NeosMod
+    public class NeosContactsSort : ResoniteMod
     {
         public override string Name => "NeosContactsSort";
         public override string Author => "hantabaru1014";
-        public override string Version => "2.0.0";
+        public override string Version => "3.0.0";
         public override string Link => "https://github.com/hantabaru1014/NeosContactsSort";
+
+        private static string BOT_USER_ID = "U-Resonite";
 
         public static ModConfiguration? config;
 
         [AutoRegisterConfigKey]
         public static readonly ModConfigurationKey<List<string>> FavoriteUsers = new ModConfigurationKey<List<string>>("FavoriteUsers", " Favorite Users", () => new List<string>());
+        [AutoRegisterConfigKey]
+        public static readonly ModConfigurationKey<bool> ShowBotOnTop = new ModConfigurationKey<bool>("Show Resonite Bot on top", "ShowBotOnTop", () => true);
+        [AutoRegisterConfigKey]
+        public static readonly ModConfigurationKey<bool> ShowMeOnTop = new ModConfigurationKey<bool>("Show your account on top", "ShowMeOnTop", () => true);
 
         public override void OnEngineInit()
         {
-#if DEBUG
-            Warn($"Extremely verbose debug logging is enabled in this build. This probably means runtime messed up and gave you a debug build.");
-#endif
             config = GetConfiguration();
 
             Harmony harmony = new Harmony("net.hantabaru1014.NeosContactsSort");
@@ -39,8 +39,8 @@ namespace NeosContactsSort
         private static class HarmonyPatches
         {
             [HarmonyPrefix]
-            [HarmonyPatch(typeof(FriendsDialog), "OnCommonUpdate", new Type[] { })]
-            public static void FriendsDialogOnCommonUpdatePrefix(ref bool ___sortList, out bool __state)
+            [HarmonyPatch(typeof(ContactsDialog), "OnCommonUpdate")]
+            public static void ContactsDialogOnCommonUpdatePrefix(ref bool ___sortList, out bool __state)
             {
                 // steal the sortList bool's value, and force it to false from Neos's perspective
                 __state = ___sortList;
@@ -48,8 +48,8 @@ namespace NeosContactsSort
             }
 
             [HarmonyPostfix]
-            [HarmonyPatch(typeof(FriendsDialog), "OnCommonUpdate", new Type[] { })]
-            public static void FriendsDialogOnCommonUpdatePostfix(bool __state, SyncRef<Slot> ____listRoot)
+            [HarmonyPatch(typeof(ContactsDialog), "OnCommonUpdate")]
+            public static void ContactsDialogOnCommonUpdatePostfix(bool __state, SyncRef<Slot> ____listRoot)
             {
                 // if Neos would have sorted (but we prevented it)
                 if (__state)
@@ -57,86 +57,74 @@ namespace NeosContactsSort
                     // we need to sort
                     ____listRoot.Target.SortChildren((slot1, slot2) =>
                     {
-                        FriendItem? component1 = slot1.GetComponent<FriendItem>();
-                        FriendItem? component2 = slot2.GetComponent<FriendItem>();
-                        Friend? friend1 = component1?.Friend;
-                        Friend? friend2 = component2?.Friend;
+                        ContactItem? component1 = slot1.GetComponent<ContactItem>();
+                        ContactItem? component2 = slot2.GetComponent<ContactItem>();
+                        Contact? contact1 = component1?.Contact;
+                        Contact? contact2 = component2?.Contact;
 
                         // nulls go last
-                        if (friend1 != null && friend2 == null) return -1;
-                        if (friend1 == null && friend2 != null) return 1;
-                        if (friend1 == null && friend2 == null) return 0;
+                        if (contact1 != null && contact2 == null) return -1;
+                        if (contact1 == null && contact2 != null) return 1;
+                        if (contact1 == null && contact2 == null) return 0;
 
-                        // friends with unread messages come first
+                        // Contacts with unread messages come first
                         int messageComparison = -component1!.HasMessages.CompareTo(component2!.HasMessages);
                         if (messageComparison != 0) return messageComparison;
 
+                        // resonite bot comes first
+                        if (config!.GetValue(ShowBotOnTop))
+                        {
+                            if (contact1!.ContactUserId == BOT_USER_ID && contact2!.ContactUserId != BOT_USER_ID) return -1;
+                            if (contact2!.ContactUserId == BOT_USER_ID && contact1!.ContactUserId != BOT_USER_ID) return 1;
+                        }
+                        
+                        // my account comes first
+                        if (config!.GetValue(ShowMeOnTop))
+                        {
+                            if (contact1!.ContactUserId == Engine.Current.Cloud.CurrentUserID && contact2!.ContactUserId != Engine.Current.Cloud.CurrentUserID) return -1;
+                            if (contact2!.ContactUserId == Engine.Current.Cloud.CurrentUserID && contact1!.ContactUserId != Engine.Current.Cloud.CurrentUserID) return 1;
+                        }
+
                         // favorite users comes second
                         var favUsers = config!.GetValue(FavoriteUsers);
-                        if (favUsers!.Contains(friend1!.FriendUserId) && !favUsers.Contains(friend2!.FriendUserId)) return -1;
-                        if (!favUsers.Contains(friend1.FriendUserId) && favUsers.Contains(friend2!.FriendUserId)) return 1;
+                        if (favUsers!.Contains(contact1!.ContactUserId) && !favUsers.Contains(contact2!.ContactUserId)) return -1;
+                        if (!favUsers.Contains(contact1.ContactUserId) && favUsers.Contains(contact2!.ContactUserId)) return 1;
 
                         // sort by online status
-                        int onlineStatusOrder = GetOrderNumber(friend1!).CompareTo(GetOrderNumber(friend2!));
+                        int onlineStatusOrder = GetOrderNumber(contact1!, component1.Data).CompareTo(GetOrderNumber(contact2!, component2.Data));
                         if (onlineStatusOrder != 0) return onlineStatusOrder;
 
-                        // neos bot comes first
-                        if (friend1!.FriendUserId == "U-Neos" && friend2!.FriendUserId != "U-Neos") return -1;
-                        if (friend2!.FriendUserId == "U-Neos" && friend1!.FriendUserId != "U-Neos") return 1;
-
                         // sort by name
-                        return string.Compare(friend1!.FriendUsername, friend2!.FriendUsername, StringComparison.CurrentCultureIgnoreCase);
+                        return string.Compare(contact1!.ContactUsername, contact2!.ContactUsername, StringComparison.CurrentCultureIgnoreCase);
                     });
-
-#if DEBUG
-                    Debug("BIG FRIEND DEBUG:");
-                    foreach (Slot slot in ____listRoot.Target.Children)
-                    {
-                        FriendItem? component = slot.GetComponent<FriendItem>();
-                        Friend? friend = component?.Friend;
-                        if (friend != null)
-                        {
-                            Debug($"  {GetOrderNumber(friend)}: \"{friend.FriendUsername}\" status={friend.FriendStatus} online={friend.UserStatus?.OnlineStatus} incoming={friend.IsAccepted}");
-                        }
-                    }
-#endif
                 }
             }
 
             [HarmonyPostfix]
-            [HarmonyPatch(typeof(NeosUIStyle), nameof(NeosUIStyle.GetStatusColor), new Type[] { typeof(Friend), typeof(Engine) })]
-            public static void NeosUIStyleGetStatusColorPostfix(Friend friend, Engine engine, ref color __result)
+            [HarmonyPatch(typeof(ContactsDialog), "UpdateSelectedContact")]
+            public static void ContactsDialogUpdateSelectedContactPostfix(ContactsDialog __instance, UIBuilder ___actionsUi)
             {
-                OnlineStatus onlineStatus = friend.UserStatus?.OnlineStatus ?? OnlineStatus.Offline;
-                if (onlineStatus == OnlineStatus.Offline && friend.FriendStatus == FriendStatus.Accepted && !friend.IsAccepted)
-                {
-                    __result = color.Yellow;
-                }
-            }
-
-            [HarmonyPostfix]
-            [HarmonyPatch(typeof(FriendsDialog), "UpdateSelectedFriend")]
-            public static void FriendsDialogUpdateSelectedFriendPostfix(FriendsDialog __instance, UIBuilder ___actionsUi)
-            {
-                if (__instance.SelectedFriend == null || __instance.SelectedFriend.FriendUserId == "U-Neos") return;
+                if (__instance.SelectedContact == null
+                    || __instance.SelectedContact.ContactUserId == BOT_USER_ID
+                    || __instance.SelectedContact.ContactUserId == Engine.Current.Cloud.CurrentUserID) return;
                 UIBuilder actionsUIBuilder = ___actionsUi;
                 var favUsers = config!.GetValue(FavoriteUsers)!;
-                if (favUsers.Contains(__instance.SelectedFriendId))
+                if (favUsers.Contains(__instance.SelectedContactId))
                 {
                     actionsUIBuilder.Button("★").LocalPressed += (IButton button, ButtonEventData eventData) =>
                     {
-                        favUsers.Remove(__instance.SelectedFriendId);
+                        favUsers.Remove(__instance.SelectedContactId);
                         config.Set(FavoriteUsers, favUsers);
-                        UpdateFriendsDialog(__instance);
+                        UpdateContactsDialog(__instance);
                     };
                 }
                 else
                 {
                     actionsUIBuilder.Button("☆").LocalPressed += (IButton button, ButtonEventData eventData) =>
                     {
-                        favUsers.Add(__instance.SelectedFriendId);
+                        favUsers.Add(__instance.SelectedContactId);
                         config.Set(FavoriteUsers, favUsers);
-                        UpdateFriendsDialog(__instance);
+                        UpdateContactsDialog(__instance);
                     };
                 }
                 var favBtnSlot = actionsUIBuilder.Root.Children.Last();
@@ -147,12 +135,12 @@ namespace NeosContactsSort
         }
 
         // lower numbers appear earlier in the list
-        private static int GetOrderNumber(Friend friend)
+        private static int GetOrderNumber(Contact contact, ContactData status)
         {
-            if (friend.FriendStatus == FriendStatus.Requested) // received requests
+            if (contact.ContactStatus == ContactStatus.Requested) // received requests
                 return 0;
-            OnlineStatus status = friend.UserStatus?.OnlineStatus ?? OnlineStatus.Offline;
-            switch (status)
+            
+            switch (status.CurrentStatus.OnlineStatus)
             {
                 case OnlineStatus.Online:
                     return 1;
@@ -161,11 +149,11 @@ namespace NeosContactsSort
                 case OnlineStatus.Busy:
                     return 3;
                 default: // Offline or Invisible
-                    if (friend.FriendStatus == FriendStatus.Accepted && !friend.IsAccepted)
+                    if (contact.ContactStatus == ContactStatus.Accepted && !contact.IsAccepted)
                     { // sent requests
                         return 4;
                     }
-                    else if (friend.FriendStatus != FriendStatus.SearchResult)
+                    else if (contact.ContactStatus != ContactStatus.SearchResult)
                     { // offline or invisible
                         return 5;
                         // unsure how people with no relation, ignored, or blocked will appear... but they'll end up here too
@@ -177,10 +165,10 @@ namespace NeosContactsSort
             }
         }
 
-        private static void UpdateFriendsDialog(FriendsDialog instance)
+        private static void UpdateContactsDialog(ContactsDialog instance)
         {
-            AccessTools.Method(typeof(FriendsDialog), "UpdateSelectedFriend").Invoke(instance, null);
-            AccessTools.Method(typeof(FriendsDialog), "OnCommonUpdate").Invoke(instance, null);
+            AccessTools.Method(typeof(ContactsDialog), "UpdateSelectedContact").Invoke(instance, null);
+            AccessTools.Method(typeof(ContactsDialog), "OnCommonUpdate").Invoke(instance, null);
         }
     }
 }
